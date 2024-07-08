8055e080dfd90000708016e1dc862b5633a98626
sunsart <sunsart7@naver.com>
2024-07-08 15:50:55 +0900

    아이디찾기, 비밀번호찾기, 특약사항수정 작업 예정

--- js/table.js
+++ js/table.js
@@ -1,4 +1,4 @@
-//checkbox 선택 >> 선택된 특약사항 컬러 변경
+// checkbox 선택 >> 선택된 특약사항 컬러 변경
 $(document).ready(function() {
   $('.checkbox').click(function() {
 		//선택된 checkbox의 같은 row의 lebel 가져오기 
@@ -13,11 +13,11 @@ $(document).ready(function() {
 	})
 });
 
-//특약 체크박스 선택후 >> 작성하기 버튼 클릭시
+// 특약 체크박스 선택후 >> 작성하기 버튼 클릭시
 $(document).ready(function() {
   $('.copy-button').click(function(e) {
 
-		//1. 테이블에서 선택한 특약내용을 textarea 에 보여주기
+		// 1. 테이블에서 선택한 특약내용을 textarea 에 보여주기
 		let contentStr = "";
 		for (let i=0; i<$('.checkbox').length; i++) {
 			if ($('.checkbox')[i].checked == true){
@@ -25,7 +25,7 @@ $(document).ready(function() {
 				contentStr += "\n";
 			}
 		}
-		document.getElementById("copy-content").innerHTML = contentStr;
+		document.getElementById("copy_content").innerHTML = contentStr;
 
 		//2. Clipboard.js를 이용하여 선택한 특약내용을 clipboard에 카피하기
 		var clipboard = new ClipboardJS(".copy-button");
--- server.js
+++ server.js
@@ -46,11 +46,64 @@ app.set('view engine', 'ejs');
 // 서버가 정적파일을 제공하도록 하기 위한 설정
 app.use(express.static(__dirname + ''));
 
+// clause-name 전역변수
+let clauseType = "";
 
 //=============================//
 app.get('/', function(req, res) {
-  res.render('index.ejs', {user:req.session.user});
-})
+  clauseType = "apt_trade";
+  if(req.session.user) {
+    //로그인 되어 있으면
+    let sql = " (SELECT cl.clause_id AS 'id', cl.title, cl.content FROM clauses AS cl WHERE cl.account_id=? AND cl.type=?) \
+                UNION ALL \
+                (SELECT BASICS.id, BASICS.title, BASICS.content \
+                FROM (SELECT * FROM basics AS ba WHERE ba.type = ?) BASICS \
+                LEFT OUTER JOIN (SELECT * FROM clauses AS cl WHERE cl.account_id = ? AND cl.type=?) CLAUSES \
+                ON BASICS.id = CLAUSES.clause_id \
+                WHERE CLAUSES.clause_id IS NULL) \
+                ORDER BY id ASC ";
+    let params = [req.session.user.id, clauseType, clauseType, req.session.user.id, clauseType];
+    conn.query(sql, params, function(err, rows) {
+      if(err) throw err;
+      res.render('index.ejs', {data:rows, user:req.session.user, type:setClauseName(clauseType)});
+    })
+  } else {
+    //로그인 되어 있지 않으면
+    let sql = "SELECT * FROM basics WHERE type=? AND title!=''";
+    conn.query(sql, clauseType, function(err, rows) {
+      if(err) throw err;
+      res.render('index.ejs', {data:rows, user:req.session.user, type:setClauseName(clauseType)});
+    })
+  }
+});
+
+//특약사항 페이지, 시멘틱 url, 네비게이션바에서 메뉴 선택시 테이블 내용 변경
+app.get('/type/:id', function(req, res) {
+  clauseType = req.params.id;
+  if(req.session.user) {
+    //로그인 되어 있으면
+    let sql = " (SELECT cl.clause_id AS 'id', cl.title, cl.content FROM clauses AS cl WHERE cl.account_id=? AND cl.type=?) \
+                UNION ALL \
+                (SELECT BASICS.id, BASICS.title, BASICS.content \
+                FROM (SELECT * FROM basics AS ba WHERE ba.type = ?) BASICS \
+                LEFT OUTER JOIN (SELECT * FROM clauses AS cl WHERE cl.account_id = ? AND cl.type=?) CLAUSES \
+                ON BASICS.id = CLAUSES.clause_id \
+                WHERE CLAUSES.clause_id IS NULL) \
+                ORDER BY id ASC ";
+    let params = [req.session.user.id, clauseType, clauseType, req.session.user.id, clauseType];
+    conn.query(sql, params, function(err, rows) {
+      if(err) throw err;
+      res.render('index.ejs', {data:rows, user:req.session.user, type:setClauseName(clauseType)});
+    })
+  } else {
+    //로그인 되어 있지 않으면
+    let sql = "SELECT * FROM basics WHERE type=? AND title!=''";
+    conn.query(sql, clauseType, function(err, rows) {
+      if(err) throw err;
+      res.render('index.ejs', {data:rows, user:req.session.user, type:setClauseName(clauseType)});
+    })
+  }
+}) 
 
 app.get('/login', function(req, res) {
   res.render('login.ejs', {user:req.session.user});
@@ -60,8 +113,48 @@ app.get('/signup', function(req, res) {
   res.render('signup.ejs', {user:req.session.user});
 })
 
+//약관 라우터
+app.get('/terms', function(req, res) {
+  res.render('terms.ejs', {user:req.session.user});
+})
+
 // 라우터 분리
 app.use('/', require('./routes/auth.js'));
+
+function setClauseName(eng) {
+  let kor;
+  if (eng == "apt_trade") kor = "아파트 매매 특약사항"
+  else if (eng == "apt_jeonse") kor = "아파트 전세 특약사항"
+  else if (eng == "apt_monthly") kor = "아파트 월세 특약사항"
+  else if (eng == "apt_monthly") kor = "아파트 월세 특약사항"
+
+  else if (eng == "officetel_trade") kor = "오피스텔 매매 특약사항"
+  else if (eng == "officetel_jeonse") kor = "오피스텔 전세 특약사항"
+  else if (eng == "officetel_monthly") kor = "오피스텔 월세 특약사항"
+
+  else if (eng == "dasedae_trade") kor = "다세대 매매 특약사항"
+  else if (eng == "dasedae_jeonse") kor = "다세대 전세 특약사항"
+  else if (eng == "dasedae_monthly") kor = "다세대 월세 특약사항"
+
+  else if (eng == "dagagu_trade") kor = "다가구 매매 특약사항"
+  else if (eng == "dagagu_jeonse") kor = "다가구 전세 특약사항"
+  else if (eng == "dagagu_monthly") kor = "다가구 월세 특약사항"
+
+  else if (eng == "oneroom_jeonse") kor = "원룸 전세 특약사항"
+  else if (eng == "oneroom_monthly") kor = "원룸 월세 특약사항"
+
+  else if (eng == "shop_trade") kor = "상가 매매 특약사항"
+  else if (eng == "shop_monthly") kor = "상가 월세 특약사항"
+
+  else if (eng == "factory_trade") kor = "공장 매매 특약사항"
+  else if (eng == "factory_monthly") kor = "공장 월세 특약사항"
+
+  else if (eng == "land_trade") kor = "토지 매매 특약사항"
+  else if (eng == "land_monthly") kor = "토지 월세 특약사항"
+
+  else if (eng == "etc") kor = "기타 특약사항"
+  return kor;
+}
 
 //=============================//
 app.listen(8080, function() {
