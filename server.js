9a7b393188e10096e2d67f86759a0cf522cf9e08
sunsart <sunsart7@naver.com>
2024-08-17 11:07:54 +0900

    배포테스트

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ let option = {
 let sessionStore = new mysqlstore(option);
 
 app.use(session({
-  secret : process.env.SESSION_SECRET,
+  secret : SESSION_SECRET,
   resave : false,
   saveUninitialized : true,
   store : sessionStore,
